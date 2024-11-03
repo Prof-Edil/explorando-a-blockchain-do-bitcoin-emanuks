9eb208f432d43daf05722572320ea1796ae965d5
emanuks <emanuels313@gmail.com>
2024-11-03 17:01:40 -0300

    fix: 004

--- submission/004.sh
+++ submission/004.sh
@@ -1,7 +1,7 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-DESCRIPTOR=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_219 -rpcpassword=cDhUmFaszHEI getdescriptorinfo "wpkh($XPUB/100)" | jq -r ".descriptor")
+DESCRIPTOR=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_219 -rpcpassword=cDhUmFaszHEI getdescriptorinfo "tr($XPUB/100)" | jq -r ".descriptor")
 
 TAPROOT_ADDRESS=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_219 -rpcpassword=cDhUmFaszHEI deriveaddresses "$DESCRIPTOR" | jq -r ".[0]")
 echo $TAPROOT_ADDRESS
